ab734405b6232ff6f749c96c3ea964c8e34ffb8b
fachturrohman <fachturrohman03@gmail.com>
2019-05-24 10:21:14 +0700

    fix v2.0

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -8,7 +8,7 @@
       </li>
       <li class="active treeview">
         <a href="/admin/tambahkendaraan">
-          <i class="fa fa-car"></i> <span>Manajemen Data</span>
+          <i class="fa fa-clipboard"></i> <span>Manajemen Data</span>
         </a>
       </li> 
     </ul>
